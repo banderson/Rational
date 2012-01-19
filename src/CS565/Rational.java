9e65ff554bdda92d578c3a8ed08189ef77907e7e
Ben Anderson <banderson87@gmail.com>
2012-01-18 19:38:59 -0500

    project init

---
+++ src/CS565/Rational.java
@@ -0,0 +1,13 @@
+package CS565;
+
+/*
+ * Write a program to test your class. 
+ * Use integer variables to represent the private instance variables of the class Ð the numerator and the denominator. 
+ * Provide a constructor which enables an object of this class to be initialized when it is declared. 
+ * The constructor should store the fraction in reduced form.  The fraction 2/4 is equivalent to one-half and would be stored in the object as 1 in the numerator and 2 in the denominator.  
+ * Provide a no-argument constructor with default values in case no initializers are provided. 
+ * */
+
+public class Rational {
+	
+}
---
+++ src/CS565/RationalTest.java
@@ -0,0 +1,15 @@
+package CS565;
+import org.junit.After;
+import org.junit.Before;
+
+public class RationalTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
