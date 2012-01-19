f272ec1b5ddd4815e7a46352f5b9ad29eaae8221
Ben Anderson <banderson87@gmail.com>
2012-01-18 20:39:25 -0500

    Implemented add * subtract two rational numbers (with limitation on same denominator)

--- src/CS565/Rational.java
+++ src/CS565/Rational.java
@@ -1,5 +1,7 @@
 package CS565;
 
+import java.lang.Exception;
+
 public class Rational {
 	private int numerator, denominator;
 	
@@ -14,7 +16,37 @@ public class Rational {
 		this.numerator = 1;
 		this.denominator = 1;
 	}
+
+	// convert to fraction string
+	public String toFractionString() {
+		return this.numerator + "/" + this.denominator;
+	}
 	
-	// The constructor should store the fraction in reduced form
-	// NOTE: apply Euclid's algorithm
+	// convert to floating point string
+	public String toFloatString() {
+		// cast each to float so we don't lose precision from ints
+		float result = (float)this.numerator / (float)this.denominator;
+		
+		return result + ""; // + "" converts it to a string 
+	}
+
+	public static Rational add(Rational one, Rational two) {
+		// temporary limitation until we get the LCD stuff done
+		if (one.denominator != two.denominator)
+			throw new RuntimeException("To add two Rationals they must have the same denominator");
+		
+		Rational result = new Rational(one.numerator + two.numerator, one.denominator);
+		
+		return result;
+	}
+
+	public static Rational subtract(Rational one, Rational two) {
+		// temporary limitation until we get the LCD stuff done
+		if (one.denominator != two.denominator)
+			throw new RuntimeException("To subtract two Rationals they must have the same denominator");
+		
+		Rational result = new Rational(one.numerator - two.numerator, one.denominator);
+		
+		return result;
+	} 
 }
--- src/CS565/RationalTest.java
+++ src/CS565/RationalTest.java
@@ -6,8 +6,12 @@ import org.junit.Test;
 
 public class RationalTest {
 
+	Rational half, whole;
+	
 	@Before
 	public void setUp() throws Exception {
+		half = new Rational(1, 2);
+		whole = new Rational();
 	}
 
 	@After
@@ -16,14 +20,66 @@ public class RationalTest {
 	
 	@Test
 	public void CanInitializeWithDefaultCtor() {
-		Rational frac = new Rational(1, 2);
-		Assert.assertNotNull("full ctor should return a valid object", frac);
+		Assert.assertNotNull("full ctor should return a valid object", half);
 	}
 	
 	@Test
 	public void CanInitializeWithoutValues() {
-		Rational frac = new Rational();
-		Assert.assertNotNull("default ctor should return a valid object", frac);
+		Assert.assertNotNull("default ctor should return a valid object", whole);
+	}
+	
+	@Test
+	public void CanReturnStringValAsFraction() {
+		Assert.assertTrue("String value should be 1/2", half.toFractionString().equals("1/2"));
+		Assert.assertTrue("String value should be 1/1", whole.toFractionString().equals("1/1"));
+	}
+	
+	@Test
+	public void CanReturnStringValAsFloat() {
+		Assert.assertTrue("String value should be 1/2, was "+ half.toFloatString(), half.toFloatString().equals("0.5"));
+		Assert.assertTrue("String value should be 1/1, was "+ whole.toFloatString(), whole.toFloatString().equals("1.0"));
+	}
+	
+	@Test
+	public void CanAddRationalNumbers() {
+		Rational another = new Rational(4,2); // overrides class whole variable
+		Rational result = Rational.add(half, another);
+		
+		Assert.assertTrue("String value should be 5/2", result.toFractionString().equals("5/2"));
+		Assert.assertTrue("String value should be 2.5", result.toFloatString().equals("2.5"));
+	}
+
+	@Test
+	public void CanSubtractRationalNumbers() {
+		Rational another = new Rational(4,2); // overrides class whole variable
+		Rational result = Rational.subtract(another, half);
+		
+		Assert.assertTrue("String value should be 3/2, was "+ result.toFractionString(), result.toFractionString().equals("3/2"));
+		Assert.assertTrue("String value should be 1.5", result.toFloatString().equals("1.5"));
+	}
+	
+	@Test
+	public void TryAddRationalWithDiffDenominatorThrowsException() {
+		try {
+			Rational.add(whole, half);
+		} catch (Exception e) { 
+			/* do nothing, it was supposed to be caught */
+			return;
+		}
+		
+		Assert.fail("Should have thrown exception since denominators are different");
+	}
+
+	@Test
+	public void TrySubtractRationalWithDiffDenominatorThrowsException() {
+		try {
+			Rational.subtract(whole, half);
+		} catch (Exception e) { 
+			/* do nothing, it was supposed to be caught */
+			return;
+		}
+		
+		Assert.fail("Should have thrown exception since denominators are different");
 	}
 
 }
