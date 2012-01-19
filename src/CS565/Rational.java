cefff438444edfdfa88c4e9c036c1a80c96733fb
Ben Anderson <banderson87@gmail.com>
2012-01-18 19:52:02 -0500

    added ctors and basic tests

--- src/CS565/Rational.java
+++ src/CS565/Rational.java
@@ -1,13 +1,20 @@
 package CS565;
 
-/*
- * Write a program to test your class. 
- * Use integer variables to represent the private instance variables of the class Ð the numerator and the denominator. 
- * Provide a constructor which enables an object of this class to be initialized when it is declared. 
- * The constructor should store the fraction in reduced form.  The fraction 2/4 is equivalent to one-half and would be stored in the object as 1 in the numerator and 2 in the denominator.  
- * Provide a no-argument constructor with default values in case no initializers are provided. 
- * */
-
 public class Rational {
+	private int numerator, denominator;
 	
+	// ctor that takes a numerator and denominator to form a Rational
+	public Rational(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	
+	// no-arg ctor to initialize a "1"
+	public Rational() {
+		this.numerator = 1;
+		this.denominator = 1;
+	}
+	
+	// The constructor should store the fraction in reduced form
+	// NOTE: apply Euclid's algorithm
 }
--- src/CS565/RationalTest.java
+++ src/CS565/RationalTest.java
@@ -1,6 +1,8 @@
 package CS565;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class RationalTest {
 
@@ -10,6 +12,18 @@ public class RationalTest {
 
 	@After
 	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void CanInitializeWithDefaultCtor() {
+		Rational frac = new Rational(1, 2);
+		Assert.assertNotNull("full ctor should return a valid object", frac);
+	}
+	
+	@Test
+	public void CanInitializeWithoutValues() {
+		Rational frac = new Rational();
+		Assert.assertNotNull("default ctor should return a valid object", frac);
 	}
 
 }
