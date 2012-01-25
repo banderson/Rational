b7c69f4765a8791ebc8840bd9a61ca3e52524125
Ben Anderson <banderson87@gmail.com>
2012-01-24 20:29:56 -0500

    MathHelper cleanup

--- src/CS565/MathHelper.java
+++ src/CS565/MathHelper.java
@@ -3,7 +3,7 @@ package CS565;
 public class MathHelper {
 	
 	// Find the Greatest Common Divisor using Euclid's Algorithm
-	// Inspired by: http://en.wikipedia.org/wiki/Greatest_common_divisor#Using_Euclid.27s_algorithm
+	// Adapted from: http://en.wikipedia.org/wiki/Greatest_common_divisor#Using_Euclid.27s_algorithm
 	public static int GreatestCommonDivisor(int a, int b) {
 		// base case: when b is zero, we've found the GCD 
 		if (b == 0) {
@@ -14,8 +14,8 @@ public class MathHelper {
 		return GreatestCommonDivisor(b, a % b);
 	}
 	
-	
-	// Inspired by: http://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
+	// Least Common Multiple, based on the GCD calculation above
+	// Adapted from: http://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
 	public static int LeastCommonMultiple(int a, int b) {
 		return (Math.abs(a * b)) / GreatestCommonDivisor(a, b);
 	}
